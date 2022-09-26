80f52d15b8762cba19a7cf3166547d39fb2b5563
Anni0007 <anirudh.baghel@konverge.ai>
2022-09-26 17:46:25 +0530

    sidebar app completed

---
+++ sidebar_project/src/context.js
@@ -0,0 +1,29 @@
+import React, { useState, useContext } from 'react'
+
+const AppContext = React.createContext()
+
+const AppProvider = ({children}) =>{
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+    const [isModalOpen, setIsModalOpen] = useState(false)
+    const openSideBar = ()=>{
+        setIsSidebarOpen(true)
+    }
+    const closeSideBar = ()=>{
+        setIsSidebarOpen(false)
+    }
+
+    const openModal = ()=>{
+        setIsModalOpen(true)
+    }
+    const closeModal = ()=>{
+        setIsModalOpen(false)
+    }
+    return <AppContext.Provider 
+    value={{isModalOpen,isSidebarOpen,openModal,closeModal,openSideBar,closeSideBar}}>{children}</AppContext.Provider>
+}
+
+export const useGlobalContext = ()=>{
+    return useContext(AppContext)
+}
+
+export {AppContext, AppProvider}
